6e668ed2dc2cb4d62848fbb1d670eba45a4edd6b
VeliaRey <abaturovaanastasia5@gmail.com>
2024-03-23 14:54:23 +0300

    Added my project

---
+++ src/App/App.jsx
@@ -0,0 +1,138 @@
+import React, { Component } from 'react'
+// import ReactDOM from "react-dom";
+import './App.css'
+
+import NewTaskForm from '../NewTaskForm/NewTaskForm'
+import Footer from '../Footer/Footer'
+import TaskList from '../TaskList/TaskList'
+
+class App extends Component {
+  
+constructor() {
+  state = {
+    todoData: [],
+    status: 'all',
+  }
+}
+  createTodoTask(label) {
+	maxId = 100;
+    return {
+      label,
+      id: this.maxId++,
+      done: false,
+      edit: false,
+      createDate: new Date(),
+    }
+  }
+
+  onDeletedTask = (id) => {
+    this.setState(({ todoData }) => {
+      const idx = todoData.findIndex((el) => el.id === id)
+
+      const newArray = [...todoData.slice(0, idx), ...todoData.slice(idx + 1)]
+      return {
+        todoData: newArray,
+      }
+    })
+  }
+
+  onAddedTask = (text) => {
+    const newItem = this.createTodoTask(text)
+
+    this.setState(({ todoData }) => {
+      const newArr = [...todoData, newItem]
+      return {
+        todoData: newArr,
+      }
+    })
+  }
+
+  onCheckedTask = (id) => {
+    this.setState(({ todoData }) => {
+      return {
+        todoData: todoData.map((el) => {
+          if (el.id === id) {
+            el.done = !el.done
+          }
+          return el
+        }),
+      }
+    })
+  }
+
+  changeStatus = (value) => {
+    this.setState(({ status }) => {
+      return {
+        status: value,
+      }
+    })
+  }
+
+  taskFilter = () => {
+    const { status, todoData } = this.state
+
+    if (status === 'Completed') {
+      return todoData.filter((el) => el.done === true)
+    } else if (status === 'Active') {
+      return todoData.filter((el) => el.done === false)
+    } else {
+      return todoData
+    }
+  }
+
+  clearCompleted = () => {
+    this.setState(({ todoData }) => {
+      const newArray = todoData.filter((el) => el.done === false)
+      return {
+        todoData: newArray,
+      }
+    })
+  }
+
+  editTask = (id) => {
+    this.setState(({ todoData }) => {
+      return {
+        todoData: todoData.map((el) => {
+          if (el.id === id) {
+            el.edit = !el.edit
+          }
+          return el
+        }),
+      }
+    })
+  }
+
+  editLabel = (label) => {
+    this.setState(({ todoData }) => {
+      return {
+        todoData: todoData.map((el) => {
+          if (el.edit) {
+            el.label = label
+          }
+          return el
+        }),
+      }
+    })
+  }
+
+  render() {
+    const doneCount = this.state.todoData.filter((el) => !el.done).length
+    console.log(this.state)
+
+    return (
+      <>
+        <NewTaskForm onAddedTask={this.onAddedTask} />
+        <TaskList
+          todos={this.taskFilter()}
+          onDeletedTask={this.onDeletedTask}
+          onCheckedTask={this.onCheckedTask}
+          editTask={this.editTask}
+          editLabel={this.editLabel}
+        />
+        <Footer doneCount={doneCount} changeStatus={this.changeStatus} clearCompleted={this.clearCompleted} />
+      </>
+    )
+  }
+}
+
+export default App
---
+++ src/Footer/Footer.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+// import ReactDOM from "react-dom";
+import PropTypes from 'prop-types'
+import TasksFilter from '../TasksFilter/TasksFilter'
+import './Footer.css'
+
+class Footer extends Component {
+  static defaultProps = {
+    changeStatus: () => {},
+    clearCompleted: () => {},
+  }
+
+  static propTypes = {
+    doneCount: PropTypes.number,
+    changeStatus: PropTypes.func,
+    clearCompleted: PropTypes.func,
+  }
+
+  render() {
+    const { doneCount, changeStatus, clearCompleted } = this.props
+
+    return (
+      <footer className="footer">
+        <span className="todo-count">{doneCount} items left</span>
+        <TasksFilter changeStatus={changeStatus} />
+        <button className="clear-completed" onClick={() => clearCompleted()}>
+          Clear completed
+        </button>
+      </footer>
+    )
+  }
+}
+
+export default Footer
